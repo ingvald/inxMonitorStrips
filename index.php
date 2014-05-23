22219878916db06a053bceb25811cc31c30d9074
ingvald <ingvald+cloud9ide@gmail.com>
2014-05-23 08:47:36 -0400

    a bit html

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-#error_reporting(E_ALL);
-#ini_set("display_errors", 1);
-?>
-hello world
+
+?><html>
+    <head>
+      <title>
+monitor strips
+      </title>
+    </head>
+  <body>
+
+
+
+  </body>
+</html>
