ce7615704e37967840c73ae86d84c4d72e068718
ingvald <ingvald+cloud9ide@gmail.com>
2014-07-25 11:57:57 +0000

    initiating feed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   //needs feed php utility
   require_once('SimplePie.compiled.php');
-
+  require_once('lib.php');
 ?><html>
     <head>
       <title>
@@ -12,6 +12,19 @@ monitor strips
 
 
 TBD
+
+<?php
+
+$feed = getInitiatedSimplePieFeed();
+
+/*
+* init simplepie feed with function
+* loop through only first, or find simple alternative
+* strip down to html with img url
+* adjust size
+* get/ show img directly using url
+*/
+?>
 
   </body>
 </html>
