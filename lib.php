7dad86681ef817fd702e58d1afdbfc17eaf71a2f
ingvald <ingvald+cloud9ide@gmail.com>
2014-07-25 11:45:00 +0000

    added initial simplepie function

---
+++ lib.php
@@ -0,0 +1,26 @@
+<?php
+
+//needs feed php utility
+require_once('SimplePie.compiled.php');
+
+//comments from simplepie example
+function getInitiatedSimplePieFeed(){
+  // We'll process this feed with all of the default options.
+  $feed = new SimplePie();
+ 
+  // Set which feed to process.
+  $feed->set_feed_url("http://www.pinterest.com/skaug/strips/rss");
+
+  // Run SimplePie.
+  $feed->init();
+
+  //$feed->set_cache_duration(1);
+ 
+  // This makes sure that the content is sent to the browser as text/html and the UTF-8 character set (since we didn't change it).
+  $feed->handle_content_type();  
+    
+  return $feed;
+}
+
+
+?>
