097ba244218f2797898edca219554162a8cd8682
ingvald <ingvald+cloud9ide@gmail.com>
2014-07-25 11:47:06 +0000

    added test for lib function

--- tests/basictest.php
+++ tests/basictest.php
@@ -2,10 +2,17 @@
 //initial tests, trying phpunit
 
 class basictest extends PHPUnit_Framework_TestCase {
+
   public function testParsingIndexJustFine(){
     $indexPage = get_include_contents("index.php");
     $this->assertContains("/html", $indexPage);
   }
+
+  public function testGetInitSimplePieFeed(){
+    include_once("lib.php");
+    $this->assertEquals(true, function_exists("getInitiatedSimplePieFeed"));
+  }
+  
 }
 
 function get_include_contents($filename) {
