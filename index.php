3cc19ff83c192e9010474f471829f9fb7ca06dd7
Ingvald Skaug <ingvald+github@gmail.com>
2016-10-20 13:02:29 +0200

    preserve proportions...

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $feed = getInitiatedSimplePieFeed($feedUrl);
 $firstFeedItem = getFirstFeedItem($feed);
 $image = getImageFromFeedItem($firstFeedItem);
 $strip = getBiggerImage($image);
-echo "   <img width=100% height=100% src=\"" . $strip . "\">";
+echo "   <img style='max-width: 100%; max-height: 100%' src=\"" . $strip . "\">";
 
 ?>
 
